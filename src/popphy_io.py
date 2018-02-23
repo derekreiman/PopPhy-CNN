4ac7e1dc8f98ad1d5384a834edc29bf8d2bae228
Derek Reiman <dreima2@uic.edu>
2018-02-23 11:35:57 -0600

    Combined IO and data preparation.

---
+++ src/popphy_io.py
@@ -0,0 +1,42 @@
+import cPickle
+import os
+
+def load_data_from_file(dset, norm, method, split):
+    
+    dir = "../data/" + dset + "/data_sets/" + norm + "/" + method + "/" + split
+    
+    f = open(dir+"/training.save", 'rb')
+    train = cPickle.load(f)
+    f.close()
+    
+    f = open(dir+"/test.save", 'rb')
+    test = cPickle.load(f)
+    f.close()
+    
+    f = open(dir+"/validation.save", 'rb')
+    validation = cPickle.load(f)
+    f.close()
+    
+    return train, test, validation
+
+def save_network(net, dir):
+    f = open(dir + "/net.save", 'wb')
+    cPickle.dump(net, f, protocol=cPickle.HIGHEST_PROTOCOL)
+    f.close()
+
+def save_network_1D(net, dir):
+    f = open(dir + "/net_1D.save", 'wb')
+    cPickle.dump(net, f, protocol=cPickle.HIGHEST_PROTOCOL)
+    f.close()
+
+def load_network(dir):
+    f = open(dir+"/net.save", 'rb')
+    dat = cPickle.load(f)
+    f.close()
+    return dat
+
+def load_network_1D(dir):
+    f = open(dir + "/net_1D.save", "rb")
+    dat = cPickle.load(f)
+    f.close()
+    return dat    
